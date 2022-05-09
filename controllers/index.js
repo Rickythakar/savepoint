070bbcb009a4e0b108fa57e3a870624510311e1b
Lex Matthews <tanukiguernica@gmail.com>
2022-05-09 14:41:38 -0400

    wrote out login routes, still need to debug

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,13 @@ const router = require('express').Router();
 
 const apiRoutes = require('./api');
 const homeRoutes = require('./homeRoutes');
+const loginRoutes = require('./loginRoutes');
+const favoritesRoutes = require('./loginRoutes');
 
-router.use('/', homeRoutes);
+router.use('/', loginRoutes);
+router.use('/favorited', favoritesRoutes);
 router.use('/api', apiRoutes);
+router.use('/home', homeRoutes);
+
 
 module.exports = router;
