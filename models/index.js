fb7bbc932ed6392181002f3bd826a070e7b68b71
Mike Sands <mikesands94@gmail.com>
2022-05-09 12:30:56 -0400

    playlist model and relationships

--- models/index.js
+++ models/index.js
@@ -3,6 +3,8 @@ const Platform= require("./Platform");
 const Genre= require("./Genre");
 const GameGenre= require("./GameGenre");
 const GamePlatform= require("./GamePlatform");
+const User = require("./User");
+const Playlist = require("./Playlist");
 
 Game.belongsToMany(Genre,{
     through: GameGenre,
@@ -22,6 +24,16 @@ Platform.belongsToMany(Game,{
 Genre.belongsToMany(Game,{
     through: GameGenre,
     foreignKey: "genre_id"
+});
+
+User.belongsToMany(Game, {
+    through: Playlist,
+    foreignKey: "user_id"
+});
+
+Game.belongsToMany(User, {
+    through: Playlist,
+    foreignKey: "game_id"
 });
 
 module.exports= {
