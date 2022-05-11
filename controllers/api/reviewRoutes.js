b619171d7a945e087e4150a74bf4d31646f4d34a
Mike Sands <mikesands94@gmail.com>
2022-05-11 10:20:37 -0400

    edit review routes

--- controllers/api/reviewRoutes.js
+++ controllers/api/reviewRoutes.js
@@ -17,12 +17,7 @@ const withAuth = require('../../utils/auth');
 
 router.post('/addreview', withAuth, async (req,res)=>{
   try{
-    const newReview= await Review.create({
-      rating: req.body.rating,
-      content: req.body.content,
-      game_id: req.body.gameId,
-      author_id: req.body.authorId,
-    })
+    const newReview= await Review.create(req.body)
     res.status(200).json(newReview);
   }
   catch(err){
