353a35a4a39d2410c18500dc23cea3042f2a24cc
Lex Matthews <tanukiguernica@gmail.com>
2022-05-14 19:02:35 -0400

    fixed single game link

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -4,7 +4,7 @@ const { User, Game } = require('../models');
 router.get('/', async (req, res) => {
   try {
     const gamesArr=[];
-    for(i=0; i<3;i++){
+    for(i=0; i<12;i++){
       let random= Math.floor(Math.random()*200);
       console.log(random);
       let showcaseItem= await Game.findByPk(random,{
