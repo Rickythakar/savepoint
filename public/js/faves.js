0f181e807e20d1f81f4cf549fd5d159230f95d2d
Lex Matthews <tanukiguernica@gmail.com>
2022-05-15 20:25:31 -0400

    fixed login/signup

--- public/js/faves.js
+++ public/js/faves.js
@@ -1,24 +1,21 @@
 const general= document.querySelector("main");
-const deleteBtn="";
-
-const updateHandler= async (event,id) =>{
-    event.preventDefault()
+const updateHandler= async (id) =>{
     const response= await axios.put(`/api/playlist/${id}`);
     if(response) window.location.reload(true);
 }
 
-const deleteHandler= async (event,id) =>{
-    event.preventDefault()
+const deleteHandler= async (id) =>{
     const response= await axios.delete(`/api/playlist/${id}/delete`);
-    response ? window.location.reload(true)
-    : console.log(response.status)
+    if(response) window.location.reload(true);
 }
 
 general.addEventListener('click',(event)=>{
+    event.preventDefault();
+    console.log(event.target);
     if(event.target.id=="completeToggle"){
-        updateHandler(event, event.target.dataset.id);
+        updateHandler(event.target.dataset.id);
     }
     if(event.target.id=="deleteBtn"){
-        deleteHandler(event, event.target.dataset.id);
+        deleteHandler(event.target.dataset.id);
     }
 });
